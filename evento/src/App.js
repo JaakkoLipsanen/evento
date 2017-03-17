00d73d2a64ce1f9f1cbca9cadc05d697f8029436
anttilip <antti.lip@gmail.com>
2017-03-17 22:47:03 +0200

    Test connecting to API

--- evento/src/App.js
+++ evento/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import UserInfo from './UserInfo.js';
 import './App.css'
 
 class App extends Component {
+	componentWillMount() {
+		this.setState({ users: [] });
+		
+		fetch('http://evento-api.herokuapp.com/users')
+		.then(response => response.json())
+		.then(users => {
+			this.setState({ users: users });
+		});
+	}
+	
 	render() {
 		return (
 			<div className="App">
-				<h4>Hello React</h4>
+				<h1>Users: </h1>
+				{this.state.users.map(user => <UserInfo key={user.id} user={user} /> )} 
 			</div>
 		);
 	}	
---
+++ evento/src/UserInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './UserInfo.css'
+
+class UserInfo extends Component {
+	render() {
+		return (
+			<div className="UserInfo">
+				<h4>{ this.props.user.name } </h4>
+				<h4>{ this.props.user.email } </h4>
+			</div>
+		);
+	}	
+}
+
+export default UserInfo;
