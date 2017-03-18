5d45e901a8f511bd203fc811c8e30cd546a7ebf0
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2017-03-18 23:47:44 +0200

    Add user id to UserInfo

--- evento/src/UserInfo.js
+++ evento/src/UserInfo.js
@@ -7,9 +7,10 @@ class UserInfo extends Component {
 			<div className="UserInfo">
 				<h4>{ this.props.user.name } </h4>
 				<h4>{ this.props.user.email } </h4>
+				<h4>{ this.props.user.id } </h4>
 			</div>
 		);
-	}	
+	}
 }
 
 export default UserInfo;
