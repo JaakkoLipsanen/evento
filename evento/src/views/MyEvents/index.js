642417a60b4d482a07699dd0e7f6451ccb2bd39b
Jaakko <jaakko.lipsanen@helsinki.fi>
2017-03-22 17:56:59 +0200

    Add MyEvents view

---
+++ evento/src/views/MyEvents/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './MyEvents.css'
+
+class MyEvents extends Component {
+	componentWillMount() {
+	}
+
+	render() {
+		return (
+			<div className="MyEvents">
+				<h1>My Events: </h1>
+			</div>
+		);
+	}
+}
+
+export default MyEvents;
