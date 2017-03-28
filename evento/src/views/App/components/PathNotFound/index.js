57415bde7a46999957fd75884b5df166181049b4
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-03-28 19:35:51 +0300

    Add react-routing, new components and 404 page (#9)
    * Add SearchBar component
    
    * Add react-router and 404 page
    
    * Fix SearchBar tests
    
    * Remove unused import
    
    * Add tests for App

---
+++ evento/src/views/App/components/PathNotFound/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './PathNotFound.css'
+
+class PathNotFound extends Component {
+	render() {
+		return (
+      <h1> 404! </h1>
+    );
+	}
+}
+
+export default PathNotFound;
