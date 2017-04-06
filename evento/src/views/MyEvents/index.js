9dc67d9d1db8064065caa27dd8b09d81255ec990
anttilip <antti.lip@gmail.com>
2017-04-07 02:40:39 +0300

    Add users events to MyEvents page

--- evento/src/views/MyEvents/index.js
+++ evento/src/views/MyEvents/index.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import './MyEvents.css'
+import Cookie from 'js-cookie';
+import EventCard from '../../components/EventCard';
+import './MyEvents.css';
 
 class MyEvents extends Component {
-	componentWillMount() {
+	constructor() {
+		super();
+		this.state = { events: [] };
+	}
+
+	componentDidMount() {
+		const userId = Cookie.get('userId');
+		const authToken = Cookie.get('auth_token');
+
+		fetch(`/users/${userId}/events`, {
+			headers: { 'Authorization': authToken }
+		}).then(response => response.json())
+		.then(events => {
+			this.setState({ events: events });
+		});
 	}
 
 	render() {
 		return (
 			<div className="MyEvents">
 				<h1>My Events: </h1>
+				<div className="event-card-list">
+					{ this.state.events.map(event =>
+						<EventCard
+							key={event.id}
+							event={event}
+							onClick={() => this.props.history.push(`/event/${event.id}`)}/>)
+					}
+				</div>
 			</div>
 		);
 	}
