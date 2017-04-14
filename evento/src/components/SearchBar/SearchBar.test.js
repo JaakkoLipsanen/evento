a42fd289e5fcd42060ea5a1018b1e6db36616170
anttilip <antti.lip@gmail.com>
2017-04-14 18:59:00 +0300

    Add tests

--- evento/src/components/SearchBar/SearchBar.test.js
+++ evento/src/components/SearchBar/SearchBar.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import SearchBar from './';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<SearchBar />, div);
+});
+
+it('calls onChange on change', () => {
+	const callback = sinon.spy();
+	const searchBar = shallow(<SearchBar onQueryChange={callback} />);
+
+	expect(callback.called).toBe(false);
+	searchBar.find('input').simulate('change');
+	expect(callback.called).toBe(true);
 });
--- evento/src/components/TabContainer/TabContainer.test.js
+++ evento/src/components/TabContainer/TabContainer.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
 import TabContainer from './';
 
-const tabs = [{ title: "test1", path: "/test1" }];
-it('renders without crashing'); //, () => {
-//	const div = document.createElement('div');
-//	ReactDOM.render(<TabContainer tabs={tabs} />, div);
-// });
+const tabs = [
+	{ title: "test1", path: "/test1" },
+	{ title: "test2", path: "/test2" }
+];
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<TabContainer tabs={tabs} />, div);
+});
+
+it('renders tab with correct title', () => {
+	const tabContainer = mount(<TabContainer tabs={tabs} />);
+	expect(tabContainer.find('Tab').at(0).text()).toEqual(tabs[0].title);
+});
+
+it('calls callback on tab change', () => {
+	const callback = sinon.spy();
+	const tabContainer = mount(<TabContainer tabs={tabs} onSelectedTabChange={callback}/>);
+	tabContainer.find('Tab').at(1).simulate('click');
+
+	expect(callback.called).toBe(true);
+	expect(tabContainer.state('selected')).toEqual(tabs[1]);
+});
+
+it('does callback on tab change', () => {
+	const callback = sinon.spy();
+	const tabContainer = mount(<TabContainer tabs={tabs} onSelectedTabChange={callback}/>);
+
+	tabContainer.find('Tab').at(1).simulate('click');
+	tabContainer.find('Tab').at(1).simulate('click');
+
+	expect(callback.calledOnce).toBe(true);
+});
