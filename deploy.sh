eec12a30a1935dce0b30100527b180df39a36f8d
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-04-24 02:39:02 +0300

    Deploy to travis only from master branch (#43)
    * Deploy to travis only on master
    
    * Try to fix travis deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "prod" ];
+then
+   $(git config --global user.email "${GIT_EMAIL}")
+   $(git config --global user.name "${GIT_COMMIT_DISPLAYNAME}")
+   $(git remote set-url origin https://${GH_USER}:${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git)
+   $(npm run deploy)
+else
+   echo "Not running from master, so not deploying to Github!"
+fi
