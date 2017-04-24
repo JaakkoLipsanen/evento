97ba50315f3869fbd69d251ecb0a193f4ddcdae0
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-04-24 03:17:31 +0300

    Hopefully fix travis deployment (again!) (#44)
    * Fix travis deployment
    
    * Test travis deployment
    
    * Test travis deployment
    
    * Test travis deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "prod" ];
+if [ "$TRAVIS_BRANCH" == "master" ] && [$TRAVIS_PULL_REQUEST == false];
 then
+   echo "Deploying to Github"
+
    $(git config --global user.email "${GIT_EMAIL}")
    $(git config --global user.name "${GIT_COMMIT_DISPLAYNAME}")
    $(git remote set-url origin https://${GH_USER}:${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git)
