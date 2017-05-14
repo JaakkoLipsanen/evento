ac8588ff1320eed53c5b257400b6011cb0564449
Jaakko Kalevi Lipsanen <jaakko.lipsanen@outlook.com>
2017-05-14 15:30:37 +0300

    Register page now shows errors on the field which the error is related to

--- evento/src/views/AuthenticationPage/components/RegisterForm/index.js
+++ evento/src/views/AuthenticationPage/components/RegisterForm/index.js
@@ -14,28 +14,47 @@ class RegisterForm extends React.Component {
 			password: '',
 			passwordConf: '',
 
-			fieldErrors: {},
-			errorMessages: []
+			fieldErrors: {}, // contains error messages for name, email, password etc
+			errorMessage: null // contains the non-field specific error message
 		};
 	}
 
 	async register() {
 		if (this.state.password !== this.state.passwordConf) {
-			this.state.fieldErrors.passwordConf = "Passwords do not match";
-			this.forceUpdate();
+			this.setState({ fieldErrors: { passwordConf: "Passwords do not match" }});
 			return;
 		}
 
 		const registerResult = await api.register(this.state.name, this.state.email, this.state.password);
 		if(registerResult.success) {
-			// After succsesiful register, sign in
+			// After succsesful register, sign in
 			const signInResult = await api.signin(this.state.email, this.state.password);
 			if(signInResult.success) {
 				this.props.onSignIn();
 			}
 		}
 		else {
-			this.setState({ errorMessages: registerResult.error.messages });
+			this.setErrors(registerResult.error);
+		}
+	}
+
+	setErrors(error) {
+		// singular error, if the problem is "Server not responding" for example
+		this.setState({ errorMessage: error.message });
+
+		// errors with the submitted fields
+		if(error.messages && error.messages.raw) {
+			const raw = error.messages.raw;
+			const getErr = (field) => field ? field[0] : undefined;
+
+			console.log(this.nameField);
+			this.setState({
+				fieldErrors: {
+					name: getErr(raw.name),
+					email: getErr(raw.email),
+					password: getErr(raw.password)
+				}
+			});
 		}
 	}
 
@@ -43,12 +62,12 @@ class RegisterForm extends React.Component {
 		return (
 			<div className="RegisterForm">
 				<form onSubmit={(e) => this.handleSubmit(e)}>
-					{this.state.errorMessages.map(error =>
-						<p className="ErrorMessage" key={error}>{error}</p>
-					)}
+					<p className="ErrorMessage">{ this.state.errorMessage }</p>
+
 					<TextField
 						type="text"
 						floatingLabelText="name"
+						errorText={this.state.fieldErrors.name}
 						value={this.state.name}
 						onChange={(evt) => this.setState({ name: evt.target.value })} />
 
@@ -56,13 +75,16 @@ class RegisterForm extends React.Component {
 					<TextField
 						type="text"
 						floatingLabelText="e-mail"
+						errorText={this.state.fieldErrors.email}
 						value={this.state.email}
+						ref={(field) => this.emailField = field }
 						onChange={(evt) => this.setState({ email: evt.target.value })} />
 
 					<br/>
 					<TextField
 						type="password"
 						floatingLabelText="password"
+						errorText={this.state.fieldErrors.password}
 						value={this.state.password}
 						onChange={(evt) => this.setState({ password: evt.target.value })} />
 
