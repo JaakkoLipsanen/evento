312d26808fc8c5d364a82bb017e1035150c08162
Jaakko Kalevi Lipsanen <jaakko.lipsanen@outlook.com>
2017-05-17 02:00:24 +0300

    Change search bar to material-ui

--- evento/src/components/SearchBar/index.js
+++ evento/src/components/SearchBar/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
 import './SearchBar.css'
 
 class SearchBar extends Component {
 	render() {
+		const styles = {
+			hintStyle: { color: "rgba(255, 255, 255, 0.825)", paddingLeft: "2px", bottom: "11px" },
+			inputStyle: { color: "rgba(255, 255, 255, 0.95)", paddingLeft: "2px" },
+			underlineStyle: { opacity: "0.825" },
+			underlineFocusStyle: { opacity: "1" },
+
+			fullWidth: true
+		};
+
 		return (
 			<div className="SearchBar">
-				<input
+				<TextField
 					type="text"
 					name="search"
-					placeholder="search events"
-					onChange={(evt) => this.props.onQueryChange(evt.target.value)}>
-				</input>
+					hintText="search events"
+					onChange={(e, val) => this.props.onQueryChange(val)}
+
+					{...styles} />
 			</div>
 		);
 	}
