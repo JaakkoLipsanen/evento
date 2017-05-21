0392b0d67bfef6bc4cd921bdc3aa3a0f4755055a
anttilip <antti.lip@gmail.com>
2017-05-22 00:34:24 +0300

    Fix some of NewEventPopup tests

--- evento/src/views/NewEventPopup/NewEventPopup.test.js
+++ evento/src/views/NewEventPopup/NewEventPopup.test.js
@@ -25,67 +25,76 @@ describe('NewEventPopup', () => {
 		renderToDOM(<NewEventPopup />, div);
 	});
 
-	it('has an error message')
-	// , async () => {
-	// 	const wrapper = await mount(<NewEventPopup />);
-	// 	wrapper.setState({ errorMessages: ['test error'] });
-	// 	expect(wrapper.find('.ErrorMessage').node).not.toBeUndefined();
-	// });
+	it('has an error message', async () => {
+		const wrapper = await mount(<NewEventPopup />);
+		wrapper.setState({ errorMessage: 'test error' });
 
-	it('sets error messages when getCategories fails')
-	// , async () => {
-	// 	api.getCategories.restore();
-	// 	sinon.stub(api, "getCategories")
-	// 		.callsFake(() => mocks.api.responses.createError({ message: "Random error" }));
-	//
-	// 	const wrapper = await mount(<NewEventPopup />);
-	//
-	// 	expect(wrapper.state('errorMessages')).not.toBeFalsy();
-	// 	expect(wrapper.state('errorMessages').length).toBe(1);
-	// 	expect(wrapper.state('errorMessages')).toContain("Random error");
-	// });
+		expect(wrapper.find('.error-message').text()).toBe('test error');
+	});
+
+	it('sets error messages when getCategories fails', async () => {
+		api.getCategories.restore();
+		sinon.stub(api, "getCategories")
+			.callsFake(() => mocks.api.responses.createError({ message: "Random error" }));
+
+		const wrapper = await mount(<NewEventPopup />);
+
+		expect(wrapper.find('.error-message').text()).toBe('Random error');
+		expect(wrapper.state('categories').length).toBe(0);
+	});
 
 	describe('form', async () => {
-		it('calls callback onSubmit')
-		// , async () => {
-		// 	const wrapper = await mount(<NewEventPopup/>);
-		// 	const callback = sinon.spy(wrapper.instance(), 'handleSubmit');
-		// 	wrapper.find('form').simulate('submit');
-		//
-		// 	expect(callback.calledOnce).toBe(true);
-		// });
+		const getTitleInput = (wrapper) => wrapper.find('TextField').at(0).find('input');
+		const getLocationInput = (wrapper) => wrapper.find('TextField').at(1).find('input');
+		const getDescriptionInput = (wrapper) => wrapper.find('TextField').at(2).find('textarea').at(1);
+		// const getCategoryInput = (wrapper) => wrapper.find('SelectField').at(0).find('input');
+		const getImageInput = (wrapper) => wrapper.find('TextField').at(4).find('input');
+		const getCreateEventButton = (wrapper) => wrapper.find('RaisedButton').at(0).find('button');
+		const getCancelButton = (wrapper) => wrapper.find('RaisedButton').at(1).find('button');
 
-		it('changes title state onChange')
-		// , async () => {
-		// 	const wrapper = await mount(<NewEventPopup />);
-		// 	wrapper.find('.title-input')
-		// 		.simulate('change', {target: {value: 'Party'}});
-		//
-		// 	expect(wrapper.state('title')).toBe('Party');
-		// });
+		it('calls createEvent on "create event" button click', async () => {
+			const wrapper = await mount(<NewEventPopup/>);
+			const callback = sinon.spy(wrapper.instance(), 'createEvent');
+			getCreateEventButton(wrapper).simulate('click');
 
-		it('changes description state onChange')
-		// , async () => {
-		// 	const wrapper = await mount(<NewEventPopup/>);
-		// 	wrapper.find('.description-input')
-		// 		.simulate('change', {target: {value: 'Biggest baddest party!'}});
-		//
-		// 	expect(wrapper.state('description')).toBe('Biggest baddest party!');
-		// });
+			expect(callback.calledOnce).toBe(true);
+		});
 
-		it('changes location state onChange')
-		// , async () => {
-		// 	const wrapper = await mount(<NewEventPopup/>);
-		// 	wrapper.find('.location-input')
-		// 		.simulate('change', {target: {value: 'My house'}});
-		//
-		// 	expect(wrapper.state('location')).toBe('My house');
-		// });
+		it('calls close() on cancel button click', async () => {
+			const wrapper = await mount(<NewEventPopup/>);
+			const callback = sinon.spy(wrapper.instance(), 'close');
+			getCancelButton(wrapper).simulate('click');
+
+			expect(callback.calledOnce).toBe(true);
+		});
+
+		it('changes title state onChange', async () => {
+			const wrapper = await mount(<NewEventPopup />);
+			getTitleInput(wrapper).simulate('change', {target: {value: 'Party'}});
+
+			expect(wrapper.state('title')).toBe('Party');
+		});
+
+		it('changes description state onChange', async () => {
+			const wrapper = await mount(<NewEventPopup/>);
+			getDescriptionInput(wrapper)
+				.simulate('change', {target: {value: 'Biggest baddest party!'}});
+
+			expect(wrapper.state('description')).toBe('Biggest baddest party!');
+		});
+
+		it('changes location state onChange', async () => {
+			const wrapper = await mount(<NewEventPopup/>);
+			getLocationInput(wrapper)
+				.simulate('change', {target: {value: 'My house'}});
+
+			expect(wrapper.state('location')).toBe('My house');
+		});
 
 		it('changes category state onChange')
 		// , async () => {
 		// 	const wrapper = await mount(<NewEventPopup/>);
-		// 	wrapper.find('.category-input')
+		// 	getCategoryInput(wrapper)
 		// 		.simulate('change', {target: {value: 'Other'}});
 		//
 		// 	expect(wrapper.state('category')).toBe('Other');
@@ -96,8 +105,8 @@ describe('NewEventPopup', () => {
 		// 	const value = moment().add(10, 'days');
 		// 	const wrapper = await mount(<NewEventPopup/>);
 		//
-		// 	wrapper.find('.start-time-picker-container input').at(0)
-		// 		.simulate('change', { target: { value: value } });
+		// 	wrapper.find('DateTimePicker').at(0).find('DatePicker')
+		// 		.simulate('change', value);
 		//
 		// 	expect(wrapper.state('startTime').isSame(value)).toBe(true);
 		// });
@@ -138,40 +147,37 @@ describe('NewEventPopup', () => {
 	// 	});
 	});
 
-	describe('handleSubmit', () => {
-		it('calls preventDefault on event')
-		// , async () => {
-		// 	const event = { preventDefault: sinon.spy() };
-		// 	const wrapper = await mount(<NewEventPopup/>);
-		// 	wrapper.instance().handleSubmit(event);
-		//
-		// 	expect(event.preventDefault.calledOnce).toBe(true);
-		// });
-
-		const createEvent = async (title, description, category, startTime, history) => {
+	describe('createEvent', () => {
+		const createEvent = async (title, description, category, startTime, location, image) => {
 			cookies.set({ user: mocks.user, auth_token: "valid" });
 			const wrapper = await mount(<NewEventPopup history={history} />);
 			wrapper.setState({ categories: [category] });
 
 			// Type all fields and submit
-			wrapper.find('input').at(0).simulate('change', { target: { value: title } });
-			wrapper.find('input').at(1).simulate('change', { target: { value: description } });
-			wrapper.find('input').at(2).simulate('change', { target: { value: category.name } });
-			wrapper.find('input').at(3).simulate('change', { target: { value: startTime } });
-			wrapper.find('form').simulate('submit');
+			wrapper.find('TextField').at(0).find('input')
+				.simulate('change', {target: {value: title}});
+			wrapper.find('TextField').at(1).find('input')
+				.simulate('change', {target: {value: location}});
+			wrapper.find('TextField').at(2).find('textarea').at(1)
+				.simulate('change', {target: {value: description}});
+			// wrapper.find('SelectField').at(0).find('input')
+			// 	.simulate('change', {target: {value: location}});
+			wrapper.find('TextField').at(4).find('input')
+				.simulate('change', {target: {value: image}});
+
+			wrapper.find('RaisedButton').at(0).find('button').simulate('click');
 			await wrapper.wait();
 
 			return wrapper;
 		};
 
-		it('redirects to MyEvents page after successful registering')
+		it('calls onCreated after successiful event creation')
 		// , async () => {
-		// 	const history = { push: sinon.spy() };
+		// 	const onCreated = sinon.spy();
 		// 	const wrapper = await createEvent(mocks.event.title, mocks.event.description,
-		// 		mocks.categories[0], DEFAULT_TIME, history);
+		// 		mocks.categories[0], DEFAULT_TIME, mocks.event.location, onCreated);
 		//
-		// 	expect(history.push.calledOnce).toBe(true);
-		// 	expect(history.push.calledWith('/events')).toBe(true);
+		// 	expect(onCreated.calledOnce).toBe(true);
 		// });
 
 		it('does not redirect on failed registering')
