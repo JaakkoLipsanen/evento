137bfdc590fbb9b310cc02f4c84291fe3a425caa
anttilip <antti.lip@gmail.com>
2017-05-22 19:23:17 +0300

    Fix AuthenticationPage tests

--- evento/src/views/AuthenticationPage/AuthenticationPage.test.js
+++ evento/src/views/AuthenticationPage/AuthenticationPage.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import AuthenticationPage from './';
 
-import { renderToDOM } from '../../test-helpers';
+import { mount, renderToDOM } from '../../test-helpers';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	renderToDOM(<AuthenticationPage />, div);
+describe('render', async () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		renderToDOM(<AuthenticationPage />, div);
+	});
+
+	it('renders signInForm by default', async () => {
+		const wrapper = await mount(<AuthenticationPage />);
+		expect(wrapper.state('showRegister')).toBe(false);
+		expect(wrapper.find('SignInForm').length).toEqual(1);
+		expect(wrapper.find('a').text()).toBe("Not yet registered? Register here");
+	});
+
+	it('renders RegisterForm after clicking link', async () => {
+		const wrapper = await mount(<AuthenticationPage />);
+		wrapper.find('a').simulate('click');
+		expect(wrapper.state('showRegister')).toBe(true);
+		expect(wrapper.find('RegisterForm').length).toEqual(1);
+		expect(wrapper.find('a').text()).toBe("Already registered? Sign in here");
+	});
 });
